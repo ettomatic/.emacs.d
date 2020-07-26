f237fcb0804c79976c8b23e5f6615e9728626b4f
ettomatic <ettore.berardi@outlook.com>
2020-07-26 16:36:47 +0100

    Adds Ocaml

---
+++ lisp/init-ocaml.el
@@ -0,0 +1,16 @@
+(require 'init-elpa)
+(require-package 'company)
+
+(require 'company)
+
+(load "~/.emacs.d/opam-user-setup.el")
+(load "~/.opam/default/share/emacs/site-lisp/tuareg-site-file.el")
+(add-to-list 'load-path "~/.opam/default/share/emacs/site-lisp/")
+
+(autoload 'merlin-mode "merlin" "Merlin mode" t)
+(add-hook 'tuareg-mode-hook 'merlin-mode)
+(add-hook 'caml-mode-hook 'merlin-mode)
+
+;(add-to-list 'eglot-server-programs '(caml-mode . ("~/.opam/default/bin/ocamllsp" "")))
+
+(provide 'init-ocaml)
