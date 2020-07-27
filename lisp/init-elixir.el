1c2abbec8b9a7517833594309d5f7056955f2a8f
ettomatic <ettore.berardi@outlook.com>
2020-07-27 22:23:05 +0100

    Elixir.

---
+++ lisp/init-elixir.el
@@ -0,0 +1,19 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
+(use-package elixir-mode
+  :ensure t
+  :defer t)
+
+;;; org-mode
+(use-package ob-elixir
+  :init
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (elixir . t)))
+  :after org)
+
+(provide 'init-elixir)
+;;; init-elixir ends here
