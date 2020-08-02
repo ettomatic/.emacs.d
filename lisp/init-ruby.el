a15c4318a6bb27c741d208a173fc8d2dc7476445
Ettore Berardi <ettore.berardi@bbc.co.uk>
2020-08-02 11:12:07 +0100

    Use enh-ruby-mode

--- lisp/init-ruby.el
+++ lisp/init-ruby.el
@@ -2,8 +2,31 @@
 ;;; Commentary:
 ;;; Code:
 
+(use-package enh-ruby-mode
+  :ensure t
+  :defer t
+  :mode (("\\.rb\\'"       . enh-ruby-mode)
+         ("\\.ru\\'"       . enh-ruby-mode)
+         ("\\.jbuilder\\'" . enh-ruby-mode)
+         ("\\.gemspec\\'"  . enh-ruby-mode)
+         ("\\.rake\\'"     . enh-ruby-mode)
+         ("Rakefile\\'"    . enh-ruby-mode)
+         ("Gemfile\\'"     . enh-ruby-mode)
+         ("Guardfile\\'"   . enh-ruby-mode)
+         ("Capfile\\'"     . enh-ruby-mode)
+         ("Vagrantfile\\'" . enh-ruby-mode))
+  :config (progn
+	    (setq enh-ruby-indent-level 2
+		  enh-ruby-add-encoding-comment-on-save nil
+		  enh-ruby-deep-indent-paren nil
+		  enh-ruby-bounce-deep-indent t
+		  enh-ruby-hanging-indent-level 2)
+      (setq enh-ruby-program "~/.asdf/shims/ruby")))
+
 ;;; RSpec
 (use-package rspec-mode
+  :ensure t
+  :defer t
   :diminish
   :commands rspec-install-snippets
   :hook (dired-mode . rspec-dired-mode)
@@ -12,12 +35,10 @@
 
 ;;; Run a Ruby process in a buffer
 (use-package inf-ruby
-  :hook ((ruby-mode . inf-ruby-minor-mode)
+  :ensure t
+  :defer t
+  :hook ((enh-ruby-mode . inf-ruby-minor-mode)
          (compilation-filter . inf-ruby-auto-enter)))
-
-(use-package robe
-  :config
-  (add-hook 'ruby-mode-hook 'robe-mode))
 
 ;;; org-mode
 (use-package ob-ruby :ensure nil :after org)
