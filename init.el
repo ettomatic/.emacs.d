a91b2989e577e458f8530b623c0e6f93195ad0b9
Ettore Berardi <ettore.berardi@bbc.co.uk>
2020-08-02 19:08:20 +0100

    Adds function to duplicate line.

--- init.el
+++ init.el
@@ -25,15 +25,15 @@
 (setq gc-cons-threshold (* 10 1000 1000))
 
 (provide 'init)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(delete-selection-mode nil)
  '(package-selected-packages
    (quote
-    (eglot org-brain use-package-ensure-system-package smex rainbow-delimiters projectile magit ido-completing-read+ golden-ratio flycheck delight crystal-mode company atom-one-dark-theme))))
+    (fish-mode use-package-hydra use-package-ensure-system-package smex rspec-mode rg rainbow-delimiters projectile polymode org-journal org-cliplink org-brain ob-elixir magit inf-ruby ido-completing-read+ idle-highlight-mode golden-ratio flycheck enh-ruby-mode elixir-mode eglot doom-themes delight crystal-mode company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
