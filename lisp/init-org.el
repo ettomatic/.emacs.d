2bdce42bcbf21b594805250dd5e5620d6c0a38f4
Ettore Berardi <ettore.berardi@bbc.co.uk>
2021-02-13 16:46:48 +0000

    org-roam.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -26,7 +26,8 @@
                    ))
   (org-archive-location "~/org/archives/%s::")
   (org-src-fontify-natively t)
-  (org-log-done 'time))
+  (org-log-done 'time)
+  (org-return-follows-link t))
 
 (setq org-directory "~/org")
 
@@ -52,6 +53,30 @@
 	      ("CANCELLED" :foreground "forest green" :weight bold)))
 
 (require 'org)
+
+;;; Open org link in the same window
+(defun org-force-open-current-window ()
+  (interactive)
+  (let ((org-link-frame-setup (quote
+                               ((vm . vm-visit-folder)
+                                (vm-imap . vm-visit-imap-folder)
+                                (gnus . gnus)
+                                (file . find-file)
+                                (wl . wl)))
+                              ))
+    (org-open-at-point)))
+;; Depending on universal argument try opening link
+(defun org-open-maybe (&optional arg)
+  (interactive "P")
+  (if arg
+      (org-open-at-point)
+    (org-force-open-current-window)
+    )
+  )
+;; Redefine file opening without clobbering universal argumnet
+(define-key org-mode-map "\C-c\C-o" 'org-open-maybe)
+(define-key org-mode-map "RET" 'org-open-maybe)
+
 
 (use-package org-capture
   :ensure nil
@@ -92,18 +117,18 @@
 
 (define-key global-map (kbd "C-c i") 'org-capture-inbox)
 
-(use-package org-journal
-  :ensure t
-  :defer f
-  :after org
-  :init
-  (global-set-key (kbd "C-c C-j") 'org-journal-new-entry)
-  :custom
-  (org-journal-dir "~/org/journal")
-  (org-journal-date-format "%A, %d %B %Y")
-  (org-journal-file-format "%Y%m%d.org")
-  (org-journal-enable-agenda-integration t)
-  (org-journal-date-prefix "#+TITLE: Daily Notes "))
+;; (use-package org-journal
+;;   :ensure t
+;;   :defer f
+;;   :after org
+;;   :init
+;;   (global-set-key (kbd "C-c C-j") 'org-journal-new-entry)
+;;   :custom
+;;   (org-journal-dir "~/org/journal")
+;;   (org-journal-date-format "%A, %d %B %Y")
+;;   (org-journal-file-format "%Y%m%d.org")
+;;   (org-journal-enable-agenda-integration t)
+;;   (org-journal-date-prefix "#+TITLE: Daily Notes "))
 
 ;; Org Brain
 (use-package org-brain :ensure t
@@ -137,5 +162,39 @@
   :config
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory "~/org/zettel")
+  (org-roam-db-location "~/.org-roam/org-roam.db")
+  (org-roam-dailies-directory "journal/")
+  (org-roam-open-at-point t)
+  (org-roam-dailies-capture-templates
+   '(("d" "default" entry
+      #'org-roam-capture--get-point
+      "* %?"
+      :file-name "journal/%<%Y-%m-%d>"
+      :head "#+title: %<%Y-%m-%d>\n\n")))
+  :config
+  (add-hook 'after-init-hook 'org-roam-mode)
+  :bind (:map org-roam-mode-map
+              (("C-c n l" . org-roam)
+               ("C-c n f" . org-roam-find-file)
+               ("C-c n g" . org-roam-graph))
+              :map org-mode-map
+              (("C-c n i" . org-roam-insert))
+              (("C-c n I" . org-roam-insert-immediate))))
+
+(use-package deft
+      :after org
+      :bind
+      ("C-c n d" . deft)
+      :custom
+      (deft-recursive t)
+      (deft-use-filter-string-for-filename t)
+      (deft-default-extension "org")
+      (deft-directory "~/org/zettel/"))
+
 (provide 'init-org)
+
 ;;; init-org ends here
