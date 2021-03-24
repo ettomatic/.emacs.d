3a016b2105cec20acd3d73202c71dcb9243403e1
Ettore Berardi <ettore.berardi@outlook.com>
2021-03-24 18:49:58 +0000

    Add Git gutter.

--- lisp/init-version-control.el
+++ lisp/init-version-control.el
@@ -8,5 +8,18 @@
   :bind (("C-x g" . magit-status)
          ("C-x C-g" . magit-status)))
 
+(use-package diff-hl
+  :ensure t
+  :hook ((after-init . global-diff-hl-mode)
+         (dired-mode . diff-hl-dired-mode))
+  :config
+  ;; https://github.com/dgutov/diff-hl#magit
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
+
+(use-package git-gutter+
+  :ensure t
+  :diminish
+  :hook (after-init . global-git-gutter+-mode))
+
 (provide 'init-version-control)
 ;;; init-version-control ends here
