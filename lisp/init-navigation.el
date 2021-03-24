ec26beb0cfe1a8e35d8a857ac9ba6d3017af75c8
Ettore Berardi <ettore.berardi@outlook.com>
2021-03-24 18:52:20 +0000

    Use Selectrum & co.

--- lisp/init-navigation.el
+++ lisp/init-navigation.el
@@ -10,7 +10,7 @@
   :custom
   (projectile-keymap-prefix (kbd "C-c C-p"))
   (projectile-mode-line '(:eval (projectile-project-name)))
-  (projectile-completion-system 'ivy)
+  (projectile-completion-system 'default)
   :config (projectile-mode))
 
 ;; Shows a list of buffers
@@ -35,53 +35,71 @@
   :init
   (rg-enable-default-bindings))
 
-;;; Global
-;; Ivy is a generic completion tool
-(use-package ivy
-  :ensure t
-  :diminish ivy-mode
-  :defer 0.9
-  :custom
-  (ivy-use-virtual-buffers t)
-  (ivy-count-format "(%d/%d) ")
-  :config
-  (use-package swiper
-    :ensure t
-    :bind (("C-s" . swiper)))
-  (use-package counsel
-    :ensure t
-    :diminish counsel-mode
-    :config (counsel-mode)
-    :bind (("C-c s" . counsel-rg) ; ripgrep
-           ("M-x"   . counsel-M-x)
-           ("C-c b" . counsel-bookmark)
-           :map ivy-minibuffer-map
-            ("<return>" . ivy-alt-done)))
-  (use-package ivy-avy
-    :ensure t)
-  (ivy-mode))
+(use-package orderless
+  :ensure t)
 
-;; A convenient interface to your recently and most frequently used commands
-;; Will work with ivy out of the box
-;; A fork of Smex
-(use-package amx
+(setq completion-styles '(orderless))
+
+(use-package marginalia
+  :ensure
+  ;; Either bind `marginalia-cycle` globally or only in the minibuffer
+  :bind (("M-A" . marginalia-cycle)
+         :map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+  :init
+  ;; Must be in the :init section of use-package such that the mode gets
+  ;; enabled right away. Note that this forces loading the package.
+  (marginalia-mode)
+
+  ;; When using Selectrum, ensure that Selectrum is refreshed when cycling annotations.
+  (advice-add #'marginalia-cycle :after
+              (lambda () (when (bound-and-true-p selectrum-mode) (selectrum-exhibit 'keep-selected)))))
+
+(use-package selectrum
+  :ensure t)
+
+;;; Prescient as default Sorting & Filtering tool
+(use-package prescient
+  :ensure t)
+
+(use-package selectrum-prescient
+  :ensure t
+  :after selectrum)
+
+(setq selectrum-prescient-enable-filtering nil)
+
+(selectrum-mode +1)
+(selectrum-prescient-mode +1)
+(prescient-persist-mode +1)
+
+(setq orderless-skip-highlighting (lambda () selectrum-is-active))
+(setq selectrum-highlight-candidates-function #'orderless-highlight-matches)
+
+(recentf-mode 1)
+
+(use-package consult
   :ensure t
   :config
-  (amx-mode 1))
+  :bind (([C-tab] . consult-buffer)
+         ("C-x C-r" . consult-recent-file)
+         ("C-s" . consult-line)
+         ("C-x C-a" . consult-ripgrep)
+         ("M-g M-g" . consult-goto-line)
+         ("M-y" . consult-yank-pop)))
 
+(require 'consult)
+
+(use-package company-prescient
+  :after company
+  :config
+  (company-prescient-mode 1))
+(setq prescient-sort-length-enable nil)
+
+;;; Avy
 (use-package avy
   :ensure t)
 
 (global-set-key (kbd "s-;") 'avy-goto-char-2)
-
-;; (use-package ivy-prescient
-;;   :ensure t
-;;   :after counsel
-;;   :config
-;;   (ivy-prescient-mode 1))
-
-;; (use-package company-prescient
-;;   :ensure t)
 
 (provide 'init-navigation)
 ;;; init-navigation ends here
