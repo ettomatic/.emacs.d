3d0ce9c12a1b8712b42715781cb7461464523f32
Ettore Berardi <ettore.berardi@outlook.com>
2021-09-04 15:31:10 +0100

    Changes on writing mode.

--- lisp/init-writing.el
+++ lisp/init-writing.el
@@ -5,31 +5,45 @@
 ;;
 ;; Writing space
 ;;
+
+;; Distraction-free screen
 (use-package olivetti
   :ensure t
-  :defer t)
-
-(add-hook
- 'olivetti-mode-hook
- 'auto-fill-mode)
-
-(add-hook
- 'olivetti-mode-hook
- 'olivetti-mode)
-
-(add-hook
- 'olivetti-mode-hook
- 'variable-pitch-mode)
-
-(add-hook
- 'olivetti-mode-hook
- 'toggle-word-wrap 1)
+  ;:defer t
+  :init
+  (setq olivetti-body-width 74)
+  :config
+  (defun writing-mode ()
+    "Distraction-free writing environment"
+    (interactive)
+    (if (equal olivetti-mode nil)
+        (progn
+          (window-configuration-to-register 1)
+          (delete-other-windows)
+          (text-scale-increase 2)
+          (variable-pitch-mode t)
+          (visual-line-mode t)
+          (auto-fill-mode t)
+          (global-hl-line-mode 0)
+          (olivetti-mode t))
+      (progn
+        (jump-to-register 1)
+        (olivetti-mode 0)
+        (variable-pitch-mode 0)
+        (global-hl-line-mode 1)
+        (text-scale-decrease 2))))
+    :bind
+    (("<f9>" . writing-mode)))
 
 (customize-set-variable 'ispell-program-name "aspell")
 (customize-set-variable 'ispell-extra-args '("--sug-mode=ultra"))
-(add-hook 'olivetti-mode-hook 'flyspell-mode)
+
+(add-hook
+ 'olivetti-mode-hook
+ 'flyspell-mode)
 
 (use-package langtool
+  :defer t
   :config
   (setq langtool-http-server-host "localhost"
         langtool-http-server-port 8010)
