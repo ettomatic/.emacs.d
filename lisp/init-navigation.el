7efd4340adf4c90a244e061fca922b705e057ceb
Ettore Berardi <ettore.berardi@outlook.com>
2021-09-25 18:59:15 +0100

    bit more cleanup.

--- lisp/init-navigation.el
+++ lisp/init-navigation.el
@@ -86,12 +86,11 @@
 (recentf-mode 1)
 (setq recentf-max-menu-items 25)
 (setq recentf-max-saved-items 25)
-;;; exclude all of the files in the no-littering directories
-;(setq recentf-exclude '("/\\.emacs\\.d/elpa/" "recentf"))
+(add-to-list 'recentf-exclude "~/.emacs.d/elpa/")
 (add-to-list 'recentf-exclude no-littering-var-directory)
 (add-to-list 'recentf-exclude no-littering-etc-directory)
 ;;; If Emacs exits abruptly for some reason the recent file list will be lost
-;;;  therefore you may wish to call `recentf-save-list` periodically, e.g. every 5
+;;;  therefore you may wish to call `recentf-save-list` periodically, e.g. every 5min
 (run-at-time nil (* 5 60) 'recentf-save-list)
 
 (use-package consult
