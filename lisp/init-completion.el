2891694d79432c02adf524b8823f0678a4dd7488
Ettore Berardi <ettore.berardi@outlook.com>
2022-10-17 22:55:54 +0100

    Update Corfu and Cape configuration.

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -3,31 +3,34 @@
 ;;; Code:
 
 (use-package corfu
-  ;; Optional customizations
-  ;;:custom
-  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-  ;;(corfu-auto t)                    ;; Enable auto completion
-  ;;(corfu-auto-delay 0.5)
-  ;;(corfu-auto-prefix 3)
-  ;; (corfu-separator ?\s)          ;; Orderless field separator
-  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
-  ;; (corfu-preselect-first nil)    ;; Disable candidate preselection
-  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-  ;; (corfu-echo-documentation nil) ;; Disable documentation in the echo area
-  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
-
+  :custom
+  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  (corfu-auto t)                    ;; Enable auto completion
+  (corfu-auto-delay 0.5)
+  (corfu-auto-prefix 3)
+  (corfu-separator ?\s)             ;; Orderless field separator
+  (corfu-quit-at-boundary nil)      ;; Never quit at completion boundary
+  (corfu-preview-current 'insert)   ;; Do not preview current candidate
+  (corfu-preselect-first nil)       ;; Disable candidate preselection
   ;; Enable Corfu only for certain modes.
   ;; :hook ((prog-mode . corfu-mode)
   ;;        (shell-mode . corfu-mode)
   ;;        (eshell-mode . corfu-mode))
-
   ;; Recommended: Enable Corfu globally.
   ;; This is recommended since Dabbrev can be used globally (M-/).
   ;; See also `corfu-excluded-modes'.
+  :bind
+  (:map corfu-map
+        ("S-SPC" . corfu-insert-separator)
+        ("TAB"     . corfu-next)
+        ([tab]     . corfu-next)
+        ("S-TAB"   . corfu-previous)
+        ([backtab] . corfu-previous)
+        ("S-<return>" . corfu-insert)
+        ("RET"     . nil))
   :init
-  (global-corfu-mode))
+  (global-corfu-mode)
+  (corfu-history-mode))
 
 ;; A few more useful configurations...
 (use-package emacs
@@ -55,19 +58,7 @@
 
 ;; Add extensions
 (use-package cape
-  ;; Bind dedicated completion commands
-  ;; Alternative prefix keys: C-c p, M-p, M-+, ...
-  ;; :bind (("C-c p p" . completion-at-point) ;; capf
-  ;;        ("C-c p t" . complete-tag)        ;; etags
-  ;;        ("C-c p d" . cape-dabbrev)        ;; or dabbrev-completion
-  ;;        ("C-c p h" . cape-history)
-  ;;        ("C-c p f" . cape-file)
-  ;;        ("C-c p k" . cape-keyword)
-  ;;        ("C-c p s" . cape-symbol)
-  ;;        ("C-c p a" . cape-abbrev)
-  ;;        ;("C-c p i" . cape-ispell)
-  ;;        ("C-c p l" . cape-line)
-  ;;        ("C-c p w" . cape-dict)
+  :defer 10
   :init
   ;; Add `completion-at-point-functions', used by `completion-at-point'.
   (add-to-list 'completion-at-point-functions #'cape-dabbrev)
@@ -82,6 +73,13 @@
   ;;(add-to-list 'completion-at-point-functions #'cape-dict)
   ;;(add-to-list 'completion-at-point-functions #'cape-symbol)
   ;;(add-to-list 'completion-at-point-functions #'cape-line)
-)
+  :config
+  ;; Silence then pcomplete capf, no errors or messages!
+  (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-silent)
+
+  ;; Ensure that pcomplete does not write to the buffer
+  ;; and behaves as a pure `completion-at-point-function'.
+  (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-purify))
+
 (provide 'init-completion)
 ;;; init-completion ends here
