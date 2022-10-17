2ce4c82682a6c9e388bbab9223f0aa21dcc176e5
Ettore Berardi <ettore.berardi@outlook.com>
2022-10-17 22:20:38 +0100

    Update theme(s).

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -5,19 +5,32 @@
 (use-package modus-themes
   :ensure t
   :init
-  (setq modus-themes-slanted-constructs t
-        modus-themes-bold-constructs nil
-        modus-themes-mode-line 'borderless
-        modus-themes-lang-checkers 'subtle
-        modus-themes-org-blocks 'gray-background
-        modus-themes-italic-constructs t)
-
+  (setq modus-themes-mode-line '(accented borderless (padding . 2) (height . 105))
+        modus-themes-italic-constructs t
+        modus-themes-fringes nil
+        modus-themes-hl-line '(accented)
+        modus-themes-markup '(bold italic intense)
+        modus-themes-org-blocks 'gray-background)
+  
   ;; Load the theme files before enabling a theme
   (modus-themes-load-themes)
   :config
   ;; Load the theme of your choice:
-  (modus-themes-load-vivendi) ;; OR (modus-themes-load-operandi)
+  (modus-themes-load-operandi) ;; OR (modus-themes-load-vivendi)
   :bind ("<f5>" . modus-themes-toggle))
+
+;; (use-package doom-themes
+;;   :ensure t
+;;   :config
+;;   ;; Global settings (defaults)
+;;   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+;;         doom-themes-enable-italic t) ; if nil, italics is universally disabled
+;;   (load-theme 'doom-ayu-light t)
+
+;;   ;; Enable flashing mode-line on errors
+;;   (doom-themes-visual-bell-config)
+;;   ;; Corrects (and improves) org-mode's native fontification.
+;;   (doom-themes-org-config))
 
 (provide 'init-theme)
 ;;; init-theme ends here
