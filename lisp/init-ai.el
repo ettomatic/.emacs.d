321bd4d46e51993d43916661c431a948c4250b60
Ettore Berardi <ettore@fastmail.com>
2025-01-26 16:00:05 +0000

    Add GPTEL.

--- lisp/init-ai.el
+++ lisp/init-ai.el
@@ -2,24 +2,20 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package kagi
-    :ensure t
-    :custom
-    (kagi-api-token (getenv "KAGI_API_TOKEN"))
-    ;; or use a function, e.g. with the password-store package:
-    ;; (kagi-api-token (lambda () (password-store-get "Kagi/API")))
+(use-package gptel
+  :ensure t
+  :custom
+  (gptel-model   'sonar-pro)
+  (gptel-backend (gptel-make-openai "Perplexity"
+                   :host "api.perplexity.ai"
+                   :key (getenv "PERPLEXITY_API_KEY")
+                   :endpoint "/chat/completions"
+                   :stream t
+                   :models '(sonar-pro)
+                   )
+                 )
+  )
 
-    ;; Univernal Summarizer settings
-    (kagi-summarizer-engine "cecil")
-    (kagi-summarizer-default-language "EN")
-    (kagi-summarizer-cache t)
-    :custom-face
-    ;; kagi-code defaults to fixed-pitch, but can be overridden as
-    ;; follows:
-    (kagi-code ((t (:inherit org-verbatim))))
-
-    ;; likewise for kagi-bold:
-    (kagi-bold ((t (:inherit modus-themes-bold)))))
 
 (provide 'init-ai)
 ;;; init-ai ends here
