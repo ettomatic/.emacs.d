56159dd4235f21aa9e49099e28171f2f473d13db
Ettore Berardi <ettore@fastmail.com>
2025-03-08 18:06:50 +0000

    howm.

--- lisp/init-notes.el
+++ lisp/init-notes.el
@@ -19,6 +19,35 @@
   ;; Pick dates, where relevant, with Org's advanced interface:
   (setq denote-date-prompt-use-org-read-date t))
 
+(require 'howm-org)
+(use-package howm
+  :ensure t
+  :init
+  ;; Where to store the files?
+  (setq howm-directory "~/org/howm")
+  ;; What format to use for the files?
+  (setq howm-file-name-format "%Y-%m-%d-%H%M%S.org")
+  (setq howm-view-title-header "*")
+  (setq howm-dtime-format "<%Y-%m-%d %a %H:%M>")
+  ;; Avoid conflicts with Org-mode by changing Howm's prefix from "C-c ,".
+  (setq howm-prefix (kbd "C-c ;"))
+  (setq howm-follow-theme t)
+  ;; Use ripgrep for fast searching.
+  (setq howm-view-use-grep t)
+  (setq howm-view-grep-command "rg")
+  (setq howm-view-grep-option "-nH --no-heading --color never")
+  (setq howm-view-grep-extended-option nil)
+  (setq howm-view-grep-fixed-option "-F")
+  (setq howm-view-grep-expr-option nil)
+  (setq howm-view-grep-file-stdin-option nil)
+  ;; Make the "comefrom links" case-insensitive.
+  (setq howm-keyword-case-fold-search t)
+  ;; Performance
+  ;(setq howm-menu-expiry-hours 1) ;; Cache menu N hours. (*4)
+  ;(setq howm-menu-refresh-after-save nil) ;; Speed up note saving.
+  :bind*
+  ;; Conveniently open the Howm menu with "C-c ; ;".
+  ("C-c ; ;" . howm-menu))
 
 (use-package consult-notes
   :ensure t
