7b951e27d484801670c1acfea88afd4bd3fa00fd
Ettore Berardi <ettore@fastmail.com>
2026-07-31 18:14:42 +0100

    Comments out kkp package.

--- lisp/init-editing.el
+++ lisp/init-editing.el
@@ -65,11 +65,11 @@
   (setq mac-right-command-modifier 'hyper)
   (setq mac-function-modifier      'super))
 
-(use-package kkp
-  :ensure t
-  :config
-  ;; (setq kkp-alt-modifier 'alt) ;; use this if you want to map the Alt keyboard modifier to Alt in Emacs (and not to Meta)
-  (global-kkp-mode +1))
+;; (use-package kkp
+;;   :ensure t
+;;   :config
+;;   ;; (setq kkp-alt-modifier 'alt) ;; use this if you want to map the Alt keyboard modifier to Alt in Emacs (and not to Meta)
+;;   (global-kkp-mode +1))
 
 (use-package vundo
   :ensure t
