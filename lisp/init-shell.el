d3e338e9a9666fb113cc671ebc4ae0c362a79c68
Ettore Berardi <ettore@fastmail.com>
2026-07-31 09:55:53 +0100

    Deduplicate kkp use-package block
    Was declared identically in both init-editing.el and init-shell.el;
    kept the one in init-editing.el since it configures global keyboard
    input handling, not shell-specific behavior.

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -26,12 +26,6 @@
 ;; For `eat-eshell-visual-command-mode'.
 (add-hook 'eshell-load-hook #'eat-eshell-visual-command-mode)
 
-(use-package kkp
-  :ensure t
-  :config
-  ;; (setq kkp-alt-modifier 'alt) ;; use this if you want to map the Alt keyboard modifier to Alt in Emacs (and not to Meta)
-  (global-kkp-mode +1))
-
 (use-package mistty
   :ensure t
   :defer t
